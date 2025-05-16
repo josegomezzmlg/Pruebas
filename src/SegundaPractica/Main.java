b2521995f1b517202842226a6999fef432cc630e
josemaria.gomez <josemaria.gomez@bosonit.com>
2025-05-16 12:00:45 +0200

    Tercer cambio eficiente

--- src/SegundaPractica/Main.java
+++ src/SegundaPractica/Main.java
@@ -1,5 +1,6 @@
 package SegundaPractica;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,34 +16,45 @@ public class Main {
             System.out.println("4. Generar notas");
             System.out.println("5. Mostrar toda la informacion");
             System.out.println("6. Salir");
-            opcion = sc.nextInt();
-            switch (opcion) {
-                case 1:
-                  gestiones.CrearClase();
-                  gestiones.mostrarClases();
-                  break;
-                  case 2:
-                      System.out.println("ATENCION -- El codigo de los alumnos siempre debe empezar por 'A'");
-                      gestiones.crearAlumno();
-                      break;
-                      case 3:
-                          System.out.println("ATENCION -- El codigo de los profesores siempre debe empezar por 'P'");
-                          gestiones.crearProfesor();
-                          break;
-                          case 4:
-                              gestiones.ponerNotas();
-                              break;
-                              case 5:
-                                  System.out.println("Introduce el codigo de la clase: ");
-                                  String codigo = sc.next();
-                                  gestiones.mostrarInformacionClase(codigo);
-                                  break;
-                                  case 6:
-                                      break;
+
+            try {
+                opcion = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcion) {
+                    case 1:
+                        gestiones.CrearClase();
+                        gestiones.mostrarClases();
+                        break;
+                            case 2:
+                                System.out.println("ATENCIÓN -- El código de los alumnos siempre debe empezar por 'A'");
+                                gestiones.crearAlumno();
+                                 break;
+                    case 3:
+                        System.out.println("ATENCIÓN -- El código de los profesores siempre debe empezar por 'P'");
+                        gestiones.crearProfesor();
+                        break;
+                    case 4:
+                        gestiones.ponerNotas();
+                        break;
+                    case 5:
+                        System.out.println("Introduce el código de la clase: ");
+                        String codigo = sc.nextLine();
+                        gestiones.mostrarInformacionClase(codigo);
+                        break;
+                    case 6:
+                        System.out.println("Saliendo del programa...");
+                        return;
+                    default:
+                        System.out.println("Opción no válida. Intenta de nuevo.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes de introducir un numero.");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error inesperado: " + e.getMessage());
             }
         }
-
-
-
     }
 }
+
